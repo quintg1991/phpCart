568c42e0e6657b139da492b6ea35219366e60f04
quintg1991@gmail.com <fglover@una.edu>
2019-02-23 09:30:41 -0600

    Commented code

--- index.php
+++ index.php
@@ -15,12 +15,17 @@ if(isset($_POST['submit']))
   // Test that the POST is going through
   // var_dump($_POST);
 
+  // Add the posted item to the SESSION
   $_SESSION['cart'][] = $_POST['cartItem'];
+
+  // Dump the contents of the cart
+  var_dump($_SESSION['cart']);
 
 }
 // If user wanted to clear the cart
 elseif(isset($_POST['clearCart']))
 {
+  // Set the SESSION to an empty array
   $_SESSION['cart'] = array();
 }
 
@@ -39,7 +44,6 @@ elseif(isset($_POST['clearCart']))
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <script src="main.js"></script>
 </head>
 <body>
   <div class="container">
@@ -65,12 +69,21 @@ elseif(isset($_POST['clearCart']))
       <div>
         <p>Current Items in Cart:</p>
         <?php
-          foreach($_SESSION['cart'] as $item){
+          // If the SESSION variable is set
+          if(isset($_SESSION['cart']))
+          {
+            // For each SESSION['cart'] item, execute the following code
+            foreach($_SESSION['cart'] as $item)
+            {
         ?>
           <ul>
-              <li><?= $item ?></li>
+            <!-- Print out the current $item as a list item -->
+            <li><?= $item ?></li>
           </ul>
-        <?php } ?>
+        <?php 
+            } // Close out the foreach
+          } // Close out the if statement
+        ?>
       </div>
     </div>
   </div>
